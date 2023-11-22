49a3a2e134c8a7ef14e75cae96081957a8df792e
Kevin Lankhuizen <kevin@lankhuizen.info>
2023-11-22 18:16:36 +0100

    Initial Building Commit

---
+++ src/BKE/Player.java
@@ -0,0 +1,37 @@
+package BKE;
+public class Player {
+    private int id;
+    private String name;
+    private char charPlayer;
+
+    public Player(int id, String name, char charPlayer) {
+        this.id = id;
+        this.name = name;
+        this.charPlayer = charPlayer;
+    }
+
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getCharPlayer(){
+        return this.charPlayer;
+    }
+
+    public void setCharPlayer(char charPlayer) {
+        this.charPlayer = charPlayer;
+    }
+}
