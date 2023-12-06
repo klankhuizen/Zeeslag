3e1400ba7951b520a36fd0217a1b666d6a2bf35a
Ivarrooks <133967476+Ivarrooks@users.noreply.github.com>
2023-12-06 16:16:03 +0100

    Update Board.java

--- src/BKE/Game/Board.java
+++ src/BKE/Game/Board.java
@@ -4,154 +4,91 @@ import BKE.Game.Player.Player;
 
 public class Board implements IBoard {
 
-    private final int boardWidth;
-    private final int boardHeight;
-    private char[][] board;
-    private Player currentPlayer;
-    private Player opponent;
+    private char[][] veld;
 
-    public Board(int boardWidth, int boardHeight) {
-        this.boardWidth = boardWidth;
-        this.boardHeight = boardHeight;
-        this.board = new char[boardWidth][boardHeight];
+    public Board() {
+        // Hier wordt er een bord van 8 bij 8 gedefineerd
+        veld = new char[8][8];
+        BoardOn();
+
     }
 
-    public boolean checkMove(int moveRow, int moveCol) {
-        return ((moveRow < boardWidth) && (moveCol < boardHeight) && (moveRow >= 0) && (moveCol >= 0));
-    }
+    private void BoardOn() {
+        // Hier wordt een loop gemaakt om het veld te activeren met lege
+        // vakjes door middel van -
+        for (int i  = 0; i < 8; i ++) {
 
-    public boolean checkWin(Player player) {
-        // row check voor win.
-        if ((this.board[0][0] == player.getCharPlayer()) && (this.board[0][1] == player.getCharPlayer()) && (this.board[0][2] == player.getCharPlayer())) {
-            return true;
-        } else if ((this.board[1][0] == player.getCharPlayer()) && (this.board[1][1] == player.getCharPlayer()) && (this.board[1][2] == player.getCharPlayer())) {
-            return true;
-        } else if ((this.board[2][0] == player.getCharPlayer()) && (this.board[2][1] == player.getCharPlayer()) && (this.board[2][2] == player.getCharPlayer())) {
-            return true;
-        }
-        // col check voor win.
-        if ((this.board[0][0] == player.getCharPlayer()) && (this.board[1][0] == player.getCharPlayer()) && (this.board[2][0] == player.getCharPlayer())) {
-            return true;
-        } else if ((this.board[0][1] == player.getCharPlayer()) && (this.board[1][1] == player.getCharPlayer()) && (this.board[2][1] == player.getCharPlayer())) {
-            return true;
-        } else if ((this.board[0][2] == player.getCharPlayer()) && (this.board[1][2] == player.getCharPlayer()) && (this.board[2][2] == player.getCharPlayer())) {
-            return true;
-        }
-        // dia rechts en links check voor win.
-        if ((this.board[0][0] == player.getCharPlayer()) && (this.board[1][1] == player.getCharPlayer()) && (this.board[2][2] == player.getCharPlayer())) {
-            return true;
-        } else return (this.board[0][2] == player.getCharPlayer()) && (this.board[1][1] == player.getCharPlayer()) && (this.board[2][0] == player.getCharPlayer());
-    }
+            for (int j = 0; j < 8; j++) {
 
-    public boolean checkGameOver(Player player){
-        return checkWin(player);
-    }
-    
-    public boolean isGameOver() {
-        // Controleerd of er iemand gewonnen heeft
-        if (checkWin(this.currentPlayer) || checkWin(this.opponent)) {
-            return true;
-        }
+                veld[i][j] = '-';
 
-        // Controleer op gelijkspel
-        if (checkBoardFull()) {
-            return true;
-        }
-        return false;
-    }
-    
-    public void checkResult(Player player){
-        if (checkWin(player)){
-            System.out.println("Speler " + player.getName() + " heeft gewonnen");
-        } else if (checkDraw(player)){
-            System.out.println("Het is een gelijk spel geworden");
-        }
-    }
-
-    // checkt of een positie vol is of niet. True -> leeg, false -> vol.
-    public boolean checkPositionFull(int moveRow, int moveCol){
-        return this.board[moveRow][moveCol] == 0;
-    }
-
-    public boolean checkBoardFull(){
-        for(int row  = 0; row < board.length; row++){
-            for (int col = 0; col < board[row].length; col++) {
-                if (this.board[row][col] == 0) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public boolean isCellEmpty(int x, int y) {
-
-        if (board[x][y] == 'a') {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean checkDraw(Player player){
-        return (checkBoardFull() && !checkWin(player));
-    }
-
-    public void makeMove(Player player, int moveRow, int moveCol){
-        if (checkMove(moveRow, moveCol)) {
-            if (checkPositionFull(moveRow, moveCol)) {
-                setChar(moveRow, moveCol, player);
             }
         }
     }
 
-    // Een zet ongedaan maken
-    public void undoMove(int x, int y) {
-        // Doormiddel van isCellEmpty leest het af of het vak leeg is
-        if (isCellEmpty(x, y)) {
-            board[x][y] = 'a';
-        }
+    private boolean juistePositie(int row, int col) {
+        // Hier wordt er gekeken of de positie juist is die gekozen werd op het veld
+        return row >= 0 && row < 8 && col >= 0 && col < 8 && veld[row][col] == '-';
+
     }
-    
-    public void printBoard(){
-        for(int row = 0; row < board.length; row++){
-            for(int col = 0; col < board[row].length; col++){
-                System.out.print(this.board[row][col] + "\t");
+
+    public void printBoard() {
+        // Hier worden de verschilende vakken letters gegeven (de bovenste rij)
+        // Dit is om het overzichtelijk te maken voor de speler in welk vak hij zijn ship
+        // plaatst en zal op schieten
+        System.out.println("  A B C D E F G H");
+        // Door middel van een loop wordt het bord uit geprint met de actuele informatie
+        for (int i = 0; i < 8; i++) {
+
+            System.out.print((i + 1) + " ");
+
+            for (int j = 0; j < 8; j++) {
+
+                System.out.print(veld[i][j] + " ");
+
             }
+
             System.out.println();
+
+        }
+
+        System.out.println();
+
+    }
+
+    public void plaatsSchip(int row, int col) {
+        // Hier wordt er gecontroleerd of de posiitie juist is.
+        // Zo ja krijgt de speler een melding dat het plaatsen gelukt is.
+        if (juistePositie(row, col)) {
+
+            veld[row][col] = 'O';
+
+            System.out.println("Schip geplaatst op positie " + locatie(row, col));
+
+        }
+
+        else {
+
+            System.out.println("je kan hier geen schip plaatsen");
+
         }
     }
 
-    public void resetBoard(){
-        for(int row  = 0; row < board.length; row++){
-            for (int col = 0; col < board[row].length; col++) {
-                this.board[row][col] = 0;
-            }
-        }
+    public boolean isValidPosition(int row, int col) {
+        // Controleer of de zet binnen het bord valt
+        // Ik weet niet of dit zal werken
+        return row >= 0 && row < 8 && col >= 0 && col < 8;
     }
 
-    public void deleteMove(int moreRow, int moveCol){
+    private String locatie(int row, int col) {
+        // Hierdoor kan de locatienaam gegeven worden van een bepaald vak
+        char colNaam = (char) ('A' + col);
+
+        return colNaam + Integer.toString(row + 1);
+
     }
 
 
 
-    public void setBoard(char[][] board) {
-        this.board = board;
-    }
-    public char[][] getBoard() {
-        return this.board;
-    }
-
-    public void setChar(int moveRow, int moveCol, Player player) {this.board[moveRow][moveCol] = player.getCharPlayer();}
-
-    public char getChar(int moveRow, int moveCol){
-        return this.board[moveRow][moveCol];
-    }
-
-    public int getBoardWidth(){
-        return this.boardWidth;
-    }
-
-    public int getBoardHeight(){
-        return this.boardHeight;
-    }
 }
+
