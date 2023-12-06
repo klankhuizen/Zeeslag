4ced659d854def9117fa690ecbc0257b5cb35562
Rick-ZXD <128495536+Rick-ZXD@users.noreply.github.com>
2023-12-06 09:51:21 +0100

    Het gemaakte werk aan het Board en de Game
    Ik weet niet zo zeer hoe ik dit mag implementeren met de bestaande code. Graag wat help hierbij

---
+++ Board.java
@@ -0,0 +1,88 @@
+public class Board {
+    private char[][] veld;
+
+    public Board() {
+        // Hier wordt er een bord van 8 bij 8 gedefineerd
+        veld = new char[8][8];
+        BoardOn();
+
+    }
+
+    private void BoardOn() {
+        // Hier wordt een loop gemaakt om het veld te activeren met lege
+        // vakjes door middel van -
+        for (int i  = 0; i < 8; i ++) {
+
+            for (int j = 0; j < 8; j++) {
+
+                veld[i][j] = '-';
+
+            }
+        }
+    }
+
+    private boolean juistePositie(int row, int col) {
+        // Hier wordt er gekeken of de positie juist is die gekozen werd op het veld
+        return row >= 0 && row < 8 && col >= 0 && col < 8 && veld[row][col] == '-';
+
+    }
+
+    public void printBoard() {
+        // Hier worden de verschilende vakken letters gegeven (de bovenste rij)
+        // Dit is om het overzichtelijk te maken voor de speler in welk vak hij zijn ship
+        // plaatst en zal op schieten
+        System.out.println("  A B C D E F G H");
+        // Door middel van een loop wordt het bord uit geprint met de actuele informatie
+        for (int i = 0; i < 8; i++) {
+
+            System.out.print((i + 1) + " ");
+
+            for (int j = 0; j < 8; j++) {
+
+                System.out.print(veld[i][j] + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+        System.out.println();
+
+    }
+
+    public void plaatsSchip(int row, int col) {
+        // Hier wordt er gecontroleerd of de posiitie juist is.
+        // Zo ja krijgt de speler een melding dat het plaatsen gelukt is.
+        if (juistePositie(row, col)) {
+
+            veld[row][col] = 'O';
+
+            System.out.println("Schip geplaatst op positie " + locatie(row, col));
+
+        }
+
+        else {
+
+            System.out.println("je kan hier geen schip plaatsen");
+
+        }
+    }
+
+    public boolean isValidPosition(int row, int col) {
+        // Controleer of de zet binnen het bord valt
+        // Ik weet niet of dit zal werken
+        return row >= 0 && row < 8 && col >= 0 && col < 8;
+    }
+
+    private String locatie(int row, int col) {
+        // Hierdoor kan de locatienaam gegeven worden van een bepaald vak
+        char colNaam = (char) ('A' + col);
+
+        return colNaam + Integer.toString(row + 1);
+
+    }
+
+
+
+}
---
+++ Game.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Game {
+    private Board playerBoard;
+    private Board opponentBoard;
+
+    public Game() {
+        playerBoard = new Board();
+        opponentBoard = new Board();
+    }
+
+    public void playGame() {
+        System.out.println("De ronde van Zeeslag zal zo beginnen");
+
+        // Schepen voor speler plaatsen
+        plaatsSchipRandom(playerBoard);
+
+        // Schepen voor tegenstander plaatsen
+        plaatsSchipRandom(opponentBoard);
+
+        // Spelronde
+        while (!isGameOver()) {
+            System.out.println("\nJouw bord:");
+            playerBoard.printBoard();
+
+            System.out.println("\nBord van de tegenstander:");
+            opponentBoard.printBoard();
+
+            // Beurt van speler
+            zetSpeler();
+
+            // Controleer of het spel voorbij is
+            if (isGameOver()) {
+                break;
+            }
+
+            // Beurt van tegenstander
+            zetTegenstander();
+        }
+
+        // Toon het resultaat
+        Resultaat();
+    }
+
+
+    private void plaatsSchipRandom(Board board) {
+        Random random = new Random();
+        // Plaatst schepen op willekeurige locaties op het bord
+        for (int i = 1; i <= 5; i++) {
+            int row = random.nextInt(8);
+            int col = random.nextInt(8);
+
+            // Controleer of de willekeurig gekozen positie geldig is
+            // zo niet, probeer opnieuw
+            while (!board.isValidPosition(row, col)) {
+                row = random.nextInt(8);
+                col = random.nextInt(8);
+            }
+
+            board.plaatsSchip(row, col);
+        }
+    }
+
+    private void zetSpeler() {
+        // De speler kan een vak kiezen om op de schieten
+        // Dit doet de speler door het nummer en de letter
+        // van de bij behorende row en col aan te geven
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jouw beurt:");
+        System.out.print("Voer de rij in (1-8): ");
+        int row = scanner.nextInt() - 1;
+
+        System.out.print("Voer de kolom in (A-H): ");
+        char colChar = scanner.next().charAt(0);
+        int col = colChar - 'A';
+    }
+
+    private void zetTegenstander() {
+        // Implementeer de logica voor de tegenstanderbeurt
+        // bijvoorbeeld door willekeurige coördinaten te genereren voor het schieten
+        // Update de borden dienovereenkomstig
+    }
+
+
+    // schepenGezonken is niet klaar en moet nog gemaakt worden.
+    private boolean isGameOver() {
+        return playerBoard.shepenGezonken() || opponentBoard.shepenGezonken();
+    }
+
+    private void Resultaat() {
+        System.out.println("Het spel is voorbij!");
+
+        if (playerBoard.shepenGezonken()) {
+            System.out.println("De tegenstander heeft gewonnen!");
+        } else {
+            System.out.println("Je hebt gewonnen!");
+        }
+    }
+}
